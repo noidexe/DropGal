651765c080128f6a1630b8f597ffeb9124617e43
Lisandro Lorea <lisandrolorea@gmail.com>
2013-03-11 10:31:14 -0300

    Small fixes and additions
    * Change image height from 300 to 375
    * Add footer: "powered by Autofolio"
    * Add 'back to site' link

--- index.php
+++ index.php
@@ -67,8 +67,11 @@
 		 	
 		 	echo "\$('.".$categories[0]."').show();\n";
 		 	?>	
+
+		 	/*$('img').hover(function () { $(this).css('height', 250);}, function () { $(this).css('height', 200);});*/
 		});
-		
+
+
 		function showCategory() {
 
 			<?php
@@ -88,12 +91,20 @@
 
 	</script>
 	<style type="text/css">
-	body { color: #e2a540; }
+		body {
+	 		color: #e2a540;
+			background: #222222
+		}
+		a:link, a:visited, a:hover, a:active {
+			color: #f3b651;
+			text-decoration: none;
+		
+		}
 	</style>
 	</head>
-	<body style="background: #222222">
-		<h1 style="color: #e2a540; border-bottom: solid 1px #e2a540"><?= $yourname ?>
-		- Portfolio</h1>
+	<body>
+		<h1 style="color: #e2a540; border-bottom: solid 1px #e2a540"><a href="http://lisandrolorea.com.ar/blog"><?= $yourname ?>
+		- Portfolio</a></h1>
 		<form action="">
 
 		<?php
@@ -106,18 +117,20 @@
 		}
 
 		?>
-		
+		<span><a href="http://lisandrolorea.com.ar/blog"> --  &laquo;Volver al Sitio&raquo;</a></span>
 		</form>
 		<div>
 			
 			<?php
 			foreach ($collection as $item) {
-				echo "<img src='https://dl.dropbox.com/u/$userid/$folder_name/" . $item['url'] . "' style='height: 200px; padding: 5px' class='" . $item['category'] . "' />&nbsp;";				
+				echo "<img src='https://dl.dropbox.com/u/$userid/$folder_name/" . $item['url'] . "' style='height: 375px; padding: 5px' class='" . $item['category'] . "' />&nbsp;";				
 			}
 			
 
 			?>
 		</div>
+		<a href="https://github.com/noidexe/autogal"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+<address>Powered by <a href="https://github.com/noidexe/autogal">Autofolio(alpha)</a> by Lisandro Lorea</address> 
 	</body>
 	
 </html>
