decf8820c6d6ac76158d33a8bb2ef1e3bb0fd072
Lisandro Lorea <lisandrolorea@gmail.com>
2013-04-16 04:50:40 -0300

    backend almost finished

--- config.php
+++ config.php
@@ -6,11 +6,20 @@
 #
 #========================*/
 
-//Your name
-$yourname = 'Lisandro Lorea';
-//Your Dropbox user id, ie. what follows https://dl.dropbox.com/u/
-$userid = '1165389';
-//The name of your autofolio folder inside Dropbox/public/
-$folder_name = 'Portfolio';
+namespace Noidexe\Autogal;
 
-?>
+class Config 
+    {
+    //Your name
+    const username = 'Lisandro Lorea';
+    //Your Dropbox user id, ie. what follows https://dl.dropbox.com/u/
+    const userid = '1165389';
+    //The name of your autofolio folder inside Dropbox/public/
+    const folder_name = 'Portfolio';
+    const max_retries = 20;
+
+    //DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
+    static function getDirURL(){
+    	return 'https://dl.dropboxusercontent.com/u/' . self::userid . '/' . self::folder_name . '/dir.txt';
+    }
+}
--- lib/php/backend.php
+++ lib/php/backend.php
@@ -1,5 +1,37 @@
 <?php
+namespace Noidexe\Autogal;
+
+require_once('../../config.php');
+require_once('Database.php');
+require_once('HTMLGenerator.php');
+
 
 //$file = file_get_contents("https://dl.dropbox.com/u/1165389/Portfolio/dir.txt") or exit("Unable to open file!");
 //echo $file;
-echo "it works!";
+
+
+$retries = 0;
+while (file_exists('../../autogal.lock') && $retries < Config::max_retries) {
+    echo 'sleeping';
+    sleep(1);
+    $retries++;
+}
+
+$filelock = fopen('../../autogal.lock', 'w');
+$dirhash = md5(file_get_contents(Config::getDirURL() ));
+
+if ( file_exists('../../autogal.lastmod') && $dirhash == file_get_contents('../../autogal.lastmod') ) {
+	;
+}
+else
+{
+	$myDatabase = new Database();
+	$myGenerator = new HTMLGenerator();
+	$myDatabase->generateFromFile(Config::getDirURL());
+	$myGenerator->generatePages( $myDatabase->getCategories(), $myDatabase->getCollection() );
+	$lastmod = fopen('../../autogal.lastmod', 'w');
+	fwrite($lastmod, $dirhash);
+	fclose($lastmod);
+}
+fclose($filelock);
+while (unlink('../../autogal.lock') === false) { ; }
